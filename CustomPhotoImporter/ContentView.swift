df8413ed026f6b8ea57db5e219d532ffd3f7e8f3
Matias Korhonen <me@matiaskorhonen.fi>
2021-08-01 12:33:06 +0300

    Initial Commit

---
+++ CustomPhotoImporter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomPhotoImporter
+//
+//  Created by Matias Korhonen on 1.8.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomPhotoImporter/CustomPhotoImporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPhotoImporterApp.swift
+//  CustomPhotoImporter
+//
+//  Created by Matias Korhonen on 1.8.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomPhotoImporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
